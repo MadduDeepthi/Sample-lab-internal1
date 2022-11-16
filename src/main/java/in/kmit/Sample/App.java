7ed95164528ec99a7a93a545e1736cbef5c92efe
Deepthi <maddudeepthi02@gmail.com>
2022-11-16 11:28:02 +0530

    sample project for lab internal1

---
+++ src/main/java/in/kmit/Sample/App.java
@@ -0,0 +1,13 @@
+package in.kmit.Sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is SE lab internal 1 exam" );
+    }
+}
